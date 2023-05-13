e5cd850bdb3682be124e73b68a1656ff19344aea
monower <sadafahmed720@gmail.com>
2023-05-13 19:31:20 +0600

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="../assets/Vue_Logo_Black.png" alt="">
+                <h1>Vue Todos</h1>
+            </div>
+        </nav>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
